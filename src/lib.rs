d3f758b3bf76bb69eaf2582cfac23c681401b590
zetsuboii <hmzkrb@gmail.com>
2022-06-30 01:15:08 +0300

    14.01

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,36 @@
 // Using deny instead of warn may break the application in future Rust versions
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
-pub struct StrSplit {}
+pub struct StrSplit {
+  remainder: &str,
+  delimiter: &str
+}
 
 impl StrSplit {
   // haystack is what we are splitting
   // delimiter is by what we are splitting
-  pub fn new (haystack: &str, delimiter: &str) -> Self {}
+  pub fn new (haystack: &str, delimiter: &str) -> Self {
+    // No need to use StrSplit for type of self
+    Self { remainder: haystack, delimiter }
+  }
 }
 
 impl Iterator for StrSplit {
   type Item = &str;
 
   // This is the only thing we need for an iterator
-  fn next(&mut self) -> Option<Self::Item>  {}
+  fn next(&mut self) -> Option<Self::Item> {
+    if let Some(next_delim) = self.remainder.find(self.delimiter) {
+      // oh my, is this legal?
+      // --re I thought this was a string and freaked out but next_delim is the byte
+      // index so it's all cool :)
+      let until_delimiter = &self.remainder[..next_delim];
+      // next_delim is of type usize because &str.find returns the byte index of the
+      // searched pattern 
+      self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+      Some(until_delimiter)
+    }
+  }
 }
 
 #[test]
