afe04510228ed16e1ef1e029df438b92369e2d01
zetsuboii <hmzkrb@gmail.com>
2022-06-30 18:14:23 +0300

    32.27

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,47 @@
 // Using deny instead of warn may break the application in future Rust versions
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
-pub struct StrSplit {
-  remainder: &str,
-  delimiter: &str
+// 'a here is a generic way to say "This variable lives for this long".
+// Giving both remainder and delimiter the same lifetime specifier, we implicitly
+// say "Both remainder and delimiter lives for the same amount of lifetime which we
+// call 'a "
+
+// Giving a generic 'a as the lifetime, we can have remainders and delimiters that
+// live longer than the StrSplit itself. Because it makes quite sense to pass a string
+// to StrSplit::new and use the remainder of it after StrSplit has been dropped.
+
+// When we are defining a generic type we can use <> right after the name we're defining
+// be it a method, a struct or an enum
+pub struct StrSplit<'a> {
+  remainder: &'a str,
+  delimiter: &'a str
 }
 
-impl StrSplit {
-  // haystack is what we are splitting
-  // delimiter is by what we are splitting
-  pub fn new (haystack: &str, delimiter: &str) -> Self {
+// Implementations are for concerete types so `impl StrSplit<'a>` mean there's a literal
+// type named `StrSplit<'a>`. In order to have generic lifetimes for impl's we have to
+// define the lifetime after `impl` keyword
+impl<'a> StrSplit<'a> {
+  // 'haystack' is what we are splitting
+  // 'delimiter' is by what we are splitting
+
+  // As we're using generic lifetimes in the StrSplit, remainder and delimiter have
+  // the lifetime of haystack and delimiter respectively
+
+  // If it comes as confusing, think about the underlying string and the desired effect
+  // of StrSplit on it, should it deallocate the string as it is dropped, or is the 
+  // lifetime of the string might be longer than the StrSplit?
+  pub fn new (haystack: &'a str, delimiter: &'a str) -> Self {
     // No need to use StrSplit for type of self
-    Self { remainder: haystack, delimiter }
+    Self {
+      remainder: haystack, 
+      delimiter
+    }
   }
 }
 
-impl Iterator for StrSplit {
-  type Item = &str;
+impl<'a> Iterator for StrSplit<'a> {
+  // Item will live as long as the remainder, we have to specify that when defining it
+  type Item = &'a str;
 
   // This is the only thing we need for an iterator
   fn next(&mut self) -> Option<Self::Item> {
@@ -29,6 +54,16 @@ impl Iterator for StrSplit {
       // searched pattern 
       self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
       Some(until_delimiter)
+    } else if self.remainder.is_empty() {
+      // TODO
+      None 
+    } else {
+
+      // Return the last partition
+      let rest = self.remainder;
+      // Now the remainder is empty
+      self.remainder = &[];
+      Some(rest)
     }
   }
 }
